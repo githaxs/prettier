ec927640782dc7f3e46883e57e8ae584b9898c7f
ghxops <51935312+ghxops@users.noreply.github.com>
2022-04-26 15:51:47 +0000

    style: formatting code with Autopep8

--- tests/acceptance/events.py
+++ tests/acceptance/events.py
@@ -1,20 +1,11 @@
 import os
 
-events = [
-    {
-        'pull_request': {
-            'head': {'ref': 'GabeL7r-patch-9', 'sha': '74ab2e085090cbf3d1edbf5b563468ab20ed1a6b'},
-            'commits': 392,
-        },
-        'repository': {
-            'full_name': 'githaxs/test'
-        },
-        'githaxs': {
-            'token': os.getenv('GITHUB_TOKEN'),
-            'subscription_level': 5,
-            'task_settings': {},
-            'full_event_name': 'pull_request.opened'
-        }
-
-    }
-]
+events = [{'pull_request': {'head': {'ref': 'GabeL7r-patch-9',
+                                     'sha': '74ab2e085090cbf3d1edbf5b563468ab20ed1a6b'},
+                            'commits': 392,
+                            },
+           'repository': {'full_name': 'githaxs/test'},
+           'githaxs': {'token': os.getenv('GITHUB_TOKEN'),
+                       'subscription_level': 5,
+                       'task_settings': {},
+                       'full_event_name': 'pull_request.opened'}}]
