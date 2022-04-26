baef2bdeac630fdf453ddb8db7f2846d24408a62
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2022-04-26 11:52:51 -0400

    fix: file endings must contain `.` (#5)

--- src/task.sh
+++ src/task.sh
@@ -2,8 +2,8 @@
 set -e
 
 run() {
-    if [[ $1 =~ .*.(js|ts|scss|css|yaml|yml|html|jsx)$ ]]; then
-        prettier --write $1
+    if [[ $1 =~ .*\.(js|ts|scss|css|yaml|yml|html|jsx)$ ]]; then
+        prettier --write "$1"
     fi
 }
 
