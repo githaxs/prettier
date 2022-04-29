34a7c48ff66b2cb57a3a54022efc0cba80e1209d
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2022-04-29 10:55:38 -0400

    feat: update task interface

--- src/task.py
+++ src/task.py
@@ -1,28 +1,15 @@
 import os
 
 from task_interfaces import SubscriptionLevels
-from task_interfaces import TaskInterface
+from task_interfaces import FormatTask
 from task_interfaces import TaskTypes
 
 
-class Task(TaskInterface):
+class Task(FormatTask):
     """
     Verifies files are formatted with prettier.
     """
 
     name = "Prettier"
-    slug = "prettier"
-    pass_summary = ""
-    pass_text = ""
-    fail_summary = "All files not formatted correctly."
-    fail_text = ""
     subscription_level = SubscriptionLevels.STARTUP
-    actions = None
-
-    can_fix = True
-    type = TaskTypes.CODE_FORMAT
     source_script_path = "%s/task.sh" % os.path.dirname(__file__)
-    handler = "task"
-
-    def execute(self, github_body):
-        pass
